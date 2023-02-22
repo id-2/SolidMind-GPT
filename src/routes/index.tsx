5066fa0ebc88664a80abc3c713ee3064259f827d
Jordy <jordy>
2023-02-22 18:05:40 +0100

    Init tailwind and daisyui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,7 @@ import "./index.css";
 export default function Home() {
   return (
     <main>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
+      <h1>test</h1>
     </main>
   );
 }
